56eb460baf5b4c16d28427bce51045666154dd8b
Yasmin Pereira Ferrari <yasmin-ferrari@altamogiana.com.br>
2025-06-09 08:14:37 -0300

    Atualizando com repositório B

--- src/components/ui/index.ts
+++
@@ -1,6 +0,0 @@
-// components/ui/index.ts
-
-export { FormInput } from './FormInput';
-export { LoadingSpinner } from '@/components/ui/loading/LoadingSpinner';
-export { ErrorMessage } from './ErrorMessage';
-export { SuccessMessage } from './SuccessMessage';
--- src/schemas/redefinirsenha/page.tsx
+++
@@ -1,7 +0,0 @@
-export {
-  resetPasswordSchema,
-  type ResetPasswordFormData
-} from '@/schemas';
-
-// eslint-disable-next-line @typescript-eslint/no-explicit-any
-export type ResetPasswordData = any;
